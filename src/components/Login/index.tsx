a6f1a99656cbf783773b2c06dd5a8f55d33ff0ad
Hannah Sangar <64546290+88Hannah@users.noreply.github.com>
2024-06-06 10:22:09 +0100

    Login error messages handled

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -7,26 +7,45 @@ import { useRouter } from "next/navigation";
 
 export default function Login() {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const [ errorMessage, setErrorMessage ] = useState("");
     const router = useRouter();
 
-    const handleRegister = async() => {
-        try {
-            await signUp(email, password);
-        } catch (error) {
-            console.error(error);
+    const errorCodes = {
+        "auth/email-already-exists": "A user already exists with this email address.",
+        "auth/email-already-in-use": "A user already exists with this email address.",
+        "auth/invalid-credential": "Sorry, these login details are not recognised.",
+        "auth/invalid-login-credential": "Sorry, these login details are not recognised.",
+        "auth/invalid-email": "Please provide a valid email address.",
+        "auth/invalid-password": "Sorry, this password is invalid. It must contain at least six characters.",
+        "auth/user-not-found": "Sorry, this user is not recognised.",
+        "auth/weak-password": "Your password must be at least six characters."
+    }
+
+    const processResponse = (error: null | object, result: null | object) => {
+        if(error) {
+            console.log(error)
+            setErrorMessage(() => {
+                const message: string = errorCodes[error.code] ? errorCodes[error.code] : "Sorry, something has gone wrong. Please try again."
+                return message
+            })
+        } else {
+            console.log(result)
+            return router.push("/")
         }
-        return router.push("/")
+    }
+
+    const handleRegister = async() => {
+        setErrorMessage("")
+        const { result, error } = await signUp(email, password);
+        processResponse(error, result)
     }
 
     const handleLogin = async() => {
-        try {
-            await signIn(email, password);
-        } catch (error) {
-            console.error(error);
-        }
-        return router.push("/")
+        setErrorMessage("")
+        const { result, error } = await signIn(email, password);
+        processResponse(error, result)
     }
 
     return (
@@ -38,7 +57,7 @@ export default function Login() {
             <button onClick={handleRegister}>Register</button>
             <button onClick={handleLogin}>Log in</button>
 
-           
+            { errorMessage && <p>{errorMessage}</p>}
         </>
     )
 }
